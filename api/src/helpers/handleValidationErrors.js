1b223953027774f47b6e28621a3651927b296268
David Ahlstom <david0673@gmail.com>
2019-10-10 18:57:45 -0700

    start of the project

---
+++ api/src/helpers/handleValidationErrors.js
@@ -0,0 +1,13 @@
+const { validationResult } = require("express-validator");
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  console.log(` errrrror${errors}`);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  next();
+};
+
+module.exports = handleValidationErrors;
---
+++ api/src/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const AuthController = require("./controllers/auth.controller");
+const PostController = require("./controllers/post.controller");
+const UserController = require("./controllers/user.controller");
+
+const app = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.use("/auth", AuthController);
+app.use("/post", PostController);
+app.use("/users", UserController);
+
+const connectDatabase = async (
+  databaseName = "Farm-Helper",
+  hostname = "localhost"
+) => {
+  const database = await mongoose.connect(
+    `mongodb://${hostname}/${databaseName}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }
+  );
+
+  if (process.env.ENV !== "test")
+    console.log(
+      `🗑️ Database connected at mongodb://${hostname}/${databaseName}...`
+    );
+
+  return database;
+};
+
+const startServer = (hostname = "0.0.0.0", port = 1337) => {
+  app.listen(port, hostname, async () => {
+    await connectDatabase();
+    if (process.env.ENV !== "test")
+      console.log(`🚀 Server started at ${hostname}:${port}...`);
+  });
+};
+
+module.exports = {
+  app,
+  connectDatabase,
+  startServer
+};
